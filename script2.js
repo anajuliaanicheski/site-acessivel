291b7e3620f9acbb348264e1d65f004bbe7acdf7
anajuliaanicheski <141340435+anajuliaanicheski@users.noreply.github.com>
2025-04-04 07:47:03 -0300

    ok

--- script2.js
+++ script2.js
@@ -7,7 +7,7 @@ document.addEventListener('DOMContentLoaded', function(){
     })
     let tamanhoAtualFonte = 1;
     const aumentaFonteBotao = document.getElementById('aumentar-fonte');
-    const diminbotaoDeAcessibilidade.addEventListener('click', function (){
+    const aumentaFonteBotao = document.getElementById('diminuir-fonte');
     aumentaFonteBotao.addEventListener('click', function(){
         tamanhoAtualFonte += 0.1;
         document.body.style.fontSize = `${tamanhoAtualFonte}rem`
